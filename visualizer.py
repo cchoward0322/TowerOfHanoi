9b5c78fbad2f7f40b545cf1f04108a7670edaf09
casscodes0322 <cashoward2006@gmail.com>
2026-08-09 16:06:45 -0600

    started visualizer (see visualizer.py)

---
+++ visualizer.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+#TO RUN WINDOW, open terminal and type: python visualizer.py
+
+#creating the main window
+root = tk.Tk()
+root.title("Tower of Hanoi: Visualizer")
+
+#setting window dimensions
+root.geometry("600x400")
+
+#add text onto the screen
+label = tk.Label(root, text = "This is a visualizer for the Tower of Hanoi problem.")
+label.place(x=10, y=50)
+
+label2 = tk.Label(root, text = "You can use this window to visualize the steps taken to solve the problem!")
+label2.place(x=10, y=70)
+
+#add button
+button = tk.Button(root, text = "Solve puzzle!", command = lambda: print("Button clicked!"))
+button.place(x=20, y=110)
+
+#keeping the window open, make sure this line ALWAYS stays at the bottom of the code
+root.mainloop()
