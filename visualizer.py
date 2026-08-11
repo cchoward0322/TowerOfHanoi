dd650a10b1e6c1ccf0ebea2f8d636dcf940dd565
Isaiah Fox <ikaifox@icloud.com>
2026-08-11 08:35:40 -0600

    drew sum pegs up

--- visualizer.py
+++ visualizer.py
@@ -20,6 +20,9 @@ canvas.pack()
 
 #root.geometry("600x400")
 
+#centers of each peg, x axis
+PEG_X = {1: 100, 2: 300, 3: 500} 
+
 #add text onto the screen
 label = tk.Label(root, text = "This is a visualizer for the Tower of Hanoi problem.")
 label.place(x=10, y=50)
@@ -30,6 +33,16 @@ label2.place(x=10, y=70)
 #add button
 button = tk.Button(root, text = "Solve puzzle!", command = lambda: print("Button clicked!"))
 button.place(x=20, y=110)
+
+#rectangle for pegs
+canvas.create_rectangle(97, 200, 103, 350, fill="gray70")
+canvas.create_rectangle(297, 200, 303, 350, fill="gray70")
+canvas.create_rectangle(497, 200, 503, 350, fill="gray70")
+
+#add rectangles for disks
+canvas.create_rectangle(55, 330, 145, 350, fill="red", outline = "black")
+#(15 + 10*disk(1)) - ignore j messing around
+
 
 #keeping the window open, make sure this line ALWAYS stays at the bottom of the code
 root.mainloop()
