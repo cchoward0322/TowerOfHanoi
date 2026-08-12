fb63efeb72c3c586f30125e6496e9db064c912aa
casscodes0322 <cashoward2006@gmail.com>
2026-08-12 12:21:19 -0600

    pretty much done!

--- hanoi.py
+++ hanoi.py
@@ -13,22 +13,22 @@ def pm(start, end):
     moves.append((start, end)) #record every move so visualizer can play later
 
 #clear pegs each call so it doesn't keep adding to moves
-def solve(n, start, end, num_pegs=3):
+def solve(n, start, end, num_peg = 3):
     moves.clear()
-    hanoi(n, start, end, num_pegs)
+    hanoi(n, start, end, num_peg)
     return moves
 
 
-def hanoi(n, start, end, num_pegs=3):
-    if n == 1: #run UI with only one disk
+def hanoi(n, start, end, num_pegs = 3):
+    if n == 1: #run UI with only one disk & BASE CASE, if met, stop the recursion
         pm(start, end)  #move the single disk directly from start to end
     else:
-        # Find an auxiliary peg (any peg that is not start or end)
+        #configuring the temporary/auxiliary peg to use for moving disks
         temp = None
-        for peg in range(1, num_pegs + 1):
-            if peg != start and peg != end:
-                temp = peg
-                break
+        for peg in range(1, num_pegs + 1): #loops through all pegs (1 - 3) to find a peg that is not the start or end peg
+            if peg != start and peg != end: #the temporary peg is the one that is not the start or end peg
+                temp = peg #said peg is now assigned at the temporary peg
+                break #exit the loop once the temporary peg is found
         
         hanoi(n - 1, start, temp, num_pegs)  #move n-1 disks from start to temp
         pm(start, end)  #move the largest disk from start to end
@@ -37,5 +37,5 @@ def hanoi(n, start, end, num_pegs=3):
 
 # needed so we did not loop
 if __name__ == "__main__":
-    answer = solve(3, 1, 3, num_pegs=3)
+    answer = solve(3, 1, 3, num_pegs = 3)
     print(answer)
